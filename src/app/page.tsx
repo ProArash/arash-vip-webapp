9c3fcbd9b298728e186eb92e3eb272e979b277ac
ProArash <arashdev78@gmail.com>
2025-09-24 12:18:05 +0330

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
 	return (
 		<div className="flex flex-col p-5 gap-5 w-full md:border border-secondary md:rounded-2xl h-full overflow-y-auto">
-			test
+			new update
 		</div>
 	);
 };
